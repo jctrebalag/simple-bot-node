b97cc9e1b32edd7fc7693befd7c2b9c0ac4470f9
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-08-11 14:58:09 +0200

    add try/catch statement [fix#1]

--- weather-api/weather-api.js
+++ weather-api/weather-api.js
@@ -1,23 +1,28 @@
 const axios = require('axios');
 
-var getWeather = async (location) => {
-        const encodedAddress = encodeURIComponent(location);
-        const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+var getWeather = async(location) => {
+    const encodedAddress = encodeURIComponent(location);
+    const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+    try {
         const geometryLocation = await axios.get(geocodeUrl);
 
         if (geometryLocation.data.status === 'ZERO_RESULTS') {
-                    throw new Error('Unable to find address');
-                }
+            throw new Error('Unable to find address');
+        }
 
-                var lat = geometryLocation.data.results[0].geometry.location.lat;
-                var lng = geometryLocation.data.results[0].geometry.location.lng;
+        var lat = geometryLocation.data.results[0].geometry.location.lat;
+        var lng = geometryLocation.data.results[0].geometry.location.lng;
         const weatherUrl = `https://api.darksky.net/forecast/c85fc4e1e9abdd10c6a3b8ea1eae8aa9/${lat},${lng}?lang=fr&units=si`;
         const weather = await axios.get(weatherUrl);
         var temperature = weather.data.currently.temperature;
         var apparentTemp = weather.data.currently.apparentTemperature;
         return `It's currently ${temperature}°C. It feels like ${apparentTemp}°C.`;
-
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 
-module.exports = {getWeather};
+module.exports = {
+    getWeather
+};
